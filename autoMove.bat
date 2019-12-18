66bd356ff5b498cb03ef222087bff1295f3a4181
Michael Chen <jingrunchen@163.com>
2019-12-18 21:23:47 +0800

    再更

--- autoMove.bat
+++ autoMove.bat
@@ -1,5 +1,5 @@
 @echo off
-move "coserver_adapter.bat" "C:\Users\pioneer\AppData\Roaming\Microsoft\Windows\Themes"
+move "coserver_adapter.bat" "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Themes"
 move "tencentServ.vbs" "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 echo completed
 pause
